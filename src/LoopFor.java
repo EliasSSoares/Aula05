e5fbea9f56de52d5b862b13ef1fd005603d19a12
Elias SSoares <elias.ssoares@yahoo.com.br>
2021-05-19 21:31:58 -0300

    Entendo o Loop for

---
+++ src/LoopFor.java
@@ -0,0 +1,16 @@
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i <= 1000; i = i + 2) {
+			
+			System.out.println(i);
+			
+		}
+		
+
+	}
+
+}
